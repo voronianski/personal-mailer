3065fa805f47985f33de5143da5d83cdd4c1bb1b
Dmitri Voronianski <dmitri.voronianski@gmail.com>
2018-01-15 19:02:39 +0100

    add nodemailer

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const cors = require('cors');
 const logger = require('morgan');
 const express = require('express');
 const address = require('network-address');
+const nodemailer = require('nodemailer');
 
 const env = process.env.NODE_ENV || 'development';
 const port = process.env.PORT || 3030;
+const gmailUser = process.env.GMAIL_USER;
+const gmailPass = process.env.GMAIL_PASS;
 
 const EMPTY_GIF = new Buffer('R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==', 'base64');
 const EMPTY_GIF_LENGTH = EMPTY_GIF.length;
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: gmailUser,
+    pass: gmailPass
+  }
+});
 
 const app = express();
 
@@ -31,6 +44,24 @@ app.use('/track.gif', (req, res) => {
   res.status(200);
   res.write(EMPTY_GIF);
   res.end();
+});
+app.use('/send', (req, res) => {
+  const mailOptions = {
+    from: gmailUser,
+    to: req.query.to,
+    subject: 'Subject of email',
+    html: '<p>Email content</p>'
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+   if (err){
+      console.log(err);
+      res.sendStatus(500);
+   } else {
+      console.log(info);
+      res.sendStatus(200);
+   }
+  });
 });
 
 app.listen(port, () => {
